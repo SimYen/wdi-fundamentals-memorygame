5a14fe7668fa5dd78a4019c183c58148e2b9f744
SYNG <user@SYNG-MacBook-Air.local>
2019-09-05 00:17:08 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,4 @@
-//console.log("Up and running!");
+console.log("Up and running!");
 
 var cards = [
 	{
@@ -31,24 +31,35 @@ function checkForMatch() {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
-	}
-}
+	};
+};
 
-function flipCard(cardId) {
+function flipCard() {
 	//display card flipped over
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	//add card flipped to cardsInPlay array
 	cardInPlay.push(cards[cardId].rank);
 	
+	this.setAttribute('src', cards[cardId].cardImage);
 
 	//check if cards flipped match
 	if (cardInPlay.length === 2) {
 		checkForMatch();
-	}	
-}
+	};	
+};
 
-//test function
-flipCard(0);
-flipCard(2);
+//create new game board
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
+};
+
+createBoard();
